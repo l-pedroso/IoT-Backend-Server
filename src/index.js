91d66f40597aad4d07e9971633dc20b08dda1ffa
Felipe94Luis <67078754+Felipe94Luis@users.noreply.github.com>
2021-03-03 13:12:15 -0300

    add icon theme extension

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const passport = require('passport');
-const path = require('path');
+
 
 // Setting up port
 let PORT = process.env.PORT || 3000;
